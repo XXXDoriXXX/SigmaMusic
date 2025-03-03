85b3a27544e06809bf22c50236b7fb828b207e63
Vadym <vadymkolomi@gmail.com>
2025-03-03 16:30:15 +0200

    fix some shit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+const isMobile = window.innerWidth <= 768;
 document.addEventListener("DOMContentLoaded", function () {
 
     loadComponent("navbar", "../html/components/navbar.html");
@@ -14,14 +16,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }, { threshold: 0.3 });
 
     hiddenElements.forEach(el => observer.observe(el));
+    if (!isMobile) {
     window.addEventListener("scroll", function () {
         if (window.scrollY > 50) {
             header.classList.add("scrolled");
         } else {
             header.classList.remove("scrolled");
         }
-    });
+    });}
 });
+
 function loadComponent(containerId, filePath) {
     fetch(filePath)
         .then(response => response.text())
@@ -55,12 +59,18 @@ function openPlayer(index) {
 
     player.play();
     isPlaying = true;
+    setTimeout(() => {
+        modal.classList.add("show");
+    }, 10);
 }
 
 function closePlayer() {
     let modal = document.getElementById("music-modal");
     modal.classList.remove("show");
     modal.style.display = "none";
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 300);
 }
 
 
@@ -118,11 +128,14 @@ document.addEventListener("click", function (event) {
     const menu = document.getElementById("nav-links");
     const menuToggle = document.querySelector(".menu-toggle");
 
-    if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
-        menu.style.opacity = "0";
-        menu.style.transform = "translateY(-20px)";
-        setTimeout(() => {
-            menu.classList.remove("show");
-        }, 400);
+
+    if (isMobile && menu.classList.contains("show")) {
+        if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+            menu.style.opacity = "0";
+            menu.style.transform = "translateY(-20px)";
+            setTimeout(() => {
+                menu.classList.remove("show");
+            }, 400);
+        }
     }
 });
